5321393e6f682d2016fe4ead56b5509ed51a077c
«Mike» <michail.v.tolmachev@gmail.com>
2022-05-28 17:37:22 +0800

    сделал lesson02

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let title = "mySales";
-let screens = "myScreens";
+let title = "project";
+let screens = "Простые, Сложные, Интерактивные";
 let screenPrice = 1000;
 let rollback = 20;
-let fullPrice = 5000;
-let adaptive = "myAdaptive";
+let fullPrice = 500000;
+let adaptive = true;
 
-alert("Hi!");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-console.log("myFirstMessege");
+console.log(screens.length);
+
+console.log("Стоимость верстки экранов" + " " + screenPrice + " " + "рублей");
+console.log("Стоимость разработки сайта" + " " + screenPrice + " " + "рублей");
+
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
