0afe58a301188ba132a9b328b678f023b030f5c2
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-03 15:01:03 +0800

    Все работает

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ let guessfunc = () => {
       alert("Верно, Вы угадали загаданное число!");
     } else if (userNum < randomNum) {
       alert("Ваше число меньше! Осталось попыток: ");
-      userNum = +prompt("Введите число");
+      userNum = prompt("Введите число");
       guessNumber();
     } else if (isNaN(userNum)) {
       alert("Можно вводить только числа");
-      userNum = +prompt("Введите число");
+      userNum = prompt("Введите число");
       guessNumber();
     } else if (userNum > randomNum) {
       alert("Ваше число больше! Осталось попыток: ");
-      userNum = +prompt("Введите число");
-      //   guessNumber();
+      userNum = prompt("Введите число");
+      guessNumber();
     }
   }
   return Res;
