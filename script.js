50a123bce63198af7127431bb35e512c007b40b6
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-03 10:13:46 +0800

    with switch

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-let randomNum = parseInt(Math.random() * 100);
-console.log(randomNum);
+"use strict";
+let userGuess;
+let gameNumber;
 
-function guessNumber(userNum) {
-  userNum = prompt("Введите число");
+gameNumber = Math.floor(Math.random() * 100);
+console.log(gameNumber);
+userGuess = prompt("Угадай число от одного до 100");
 
-  if (isNaN(userNum)) {
-    alert("Можно вводить только числа");
-    guessNumber(userNum);
+const isNumber = function (num) {
+  return !isNaN(parseFloat(num)) && isFinite(num);
+};
+const game = function () {
+  switch (true) {
+    case userGuess > gameNumber:
+      userGuess = prompt("Загаданное число меньше");
+      game();
+      break;
+    case userGuess < gameNumber && userGuess > 1:
+      userGuess = prompt("Загаданное число больше");
+      game();
+      break;
+    case !isNumber(userGuess) && userGuess != null:
+      userGuess = prompt("Введи число!");
+      game();
+      break;
+    case userGuess == gameNumber:
+      userGuess = alert("Поздравляю, Вы угадали!!!");
+      game();
+      break;
+    case userGuess == null:
+      userGuess = alert("Игра окончена");
+      break;
+    default:
+      game();
+      break;
   }
+};
 
-  if (userNum === null) {
-    alert("Игра окончена");
-    return;
-  }
-
-  if (userNum == randomNum) {
-    alert("Верно, Вы угадали загаданное число!");
-    return;
-  } else if (userNum < randomNum) {
-    alert("Ваше число меньше");
-    guessNumber(userNum);
-  } else if (userNum > randomNum) {
-    alert("Ваше число больше");
-    guessNumber(userNum);
-  }
-}
-
-guessNumber();
+game();
