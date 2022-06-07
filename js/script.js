0f613f58b235169e79eb0021a42df5fab8851659
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-07 19:20:56 +0800

    начало

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const title = document.getElementsByTagName("h1");
+// console.log(title[0]);
+
+// const btn = document.getElementsByClassName("handler_btn");
+// console.log(btn[(0, 1)]);
+
+// const btnPlus = document.querySelector(".screen-btn");
+// console.log(btnPlus);
+
+// const percent = document.querySelectorAll(
+//   ".main-controls__item  other-items percent"
+// );
+// console.log(percent);
+
+// const number = document.querySelectorAll(
+//   ".main-controls__item other-items number"
+// );
+// console.log(number);
+
+// const inpuType = document.querySelector(".main-controls__item > div > input");
+// console.log(inpuType);
+
+// const span = document.querySelector(".rollback > span");
+// console.log(span);
