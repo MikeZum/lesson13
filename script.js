135c8523263dabdc176a81197264e73c8b4196c2
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-08 20:04:53 +0800

    Lesson10Порядок глав готово

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ deleteAdv.remove();
 const addChapter = document.querySelectorAll(".book > ul > li");
 console.log(addChapter);
 
+// Порядок глав во второй книге
 addChapter[1].after(addChapter[6]);
 addChapter[1].after(addChapter[3]);
 addChapter[6].after(addChapter[8]);
@@ -25,6 +26,14 @@ addChapter[8].after(addChapter[4]);
 addChapter[4].after(addChapter[5]);
 addChapter[5].after(addChapter[7]);
 addChapter[7].after(addChapter[9]);
+
+// Порядок глав в пятой книге
+addChapter[47].after(addChapter[55]);
+addChapter[55].after(addChapter[49]);
+addChapter[49].after(addChapter[50]);
+addChapter[50].after(addChapter[48]);
+addChapter[48].after(addChapter[52]);
+addChapter[52].after(addChapter[53]);
 
 // addChapter[2].before(addChapter[4]);
 // addChapter[2].before(addChapter[3]);
