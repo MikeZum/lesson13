e33da84f62c12accd802a3401aa8f15035990fe4
«Mike» <michail.v.tolmachev@gmail.com>
2022-06-13 17:41:25 +0800

    lesson13

---
+++ script.js
@@ -0,0 +1,61 @@
+const todoControl = document.querySelector(".todo-control");
+const headerInput = document.querySelector(".header-input");
+const todoList = document.querySelector(".todo-list");
+const todoCompleted = document.querySelector(".todo-completed");
+
+const toDoData = [
+  // {
+  //   text: "Сварить кофе",
+  //   completed: false,
+  // },
+  // {
+  //   text: "Помыть посуду",
+  //   completed: true,
+  // },
+];
+
+const render = function () {
+  todoList.innerHTML = "";
+  todoCompleted.innerHTML = "";
+
+  toDoData.forEach(function (item) {
+    const li = document.createElement("li");
+
+    li.classList.add("todo-item");
+
+    li.innerHTML =
+      '<span class="text-todo">' +
+      item.text +
+      "</span>" +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      "</div>";
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    // console.log(li.querySelector(".todo-complete"));
+    li.querySelector(".todo-complete").addEventListener("click", function () {
+      item.completed = !item.completed;
+      render();
+    });
+  });
+};
+
+todoControl.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const newToDo = {
+    text: headerInput.value,
+    completed: false,
+  };
+
+  toDoData.push(newToDo);
+  headerInput.value = "";
+
+  render();
+});
